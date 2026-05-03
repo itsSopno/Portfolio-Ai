677e68ff9c81774f22402e02e6b8ac867d07df33
itsSopno <sopno263@gmail.com>
2026-05-03 19:12:47 +0600

    save changes

--- src/controller/geminiController.ts
+++ src/controller/geminiController.ts
@@ -1,3 +1,20 @@
 import { Request, Response } from "express";
 import { gerGenAi } from "../Gemini/gemini";
 import Project from "../Models/ProjectModel";
+
+/**
+ * @name Project Helper 
+ * @description Controller for ai 
+ * 
+ */
+
+export const ProjectDetailController = async(req:Request,res:Response)=>{
+    try{
+    
+    }
+    catch(e)
+    {
+        console.log(e); 
+    } 
+}
+
