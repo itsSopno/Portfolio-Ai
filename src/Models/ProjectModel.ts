d3ab88deaa6799da3f09bda51ed727269163b19d
itsSopno <sopno263@gmail.com>
2026-05-03 18:08:06 +0600

    save changes

--- src/Models/ProjectModel.ts
+++ src/Models/ProjectModel.ts
@@ -1,2 +1,55 @@
 import { Document,model,Schema } from "mongoose";
 
+
+interface IProject extends Document{
+    name:string,
+    image:string[],
+    livelink:string,
+    github:string,
+description:string,
+date:Date,
+category:string,
+techStack:string[],
+featured:boolean,
+}
+const projectSchema = new Schema<IProject>({
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:[String],
+        required:true
+    },
+    livelink:{
+        type:String,
+        required:true
+    },
+    github:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    techStack:{
+        type:[String],
+        required:true
+    },
+    featured:{
+        type:Boolean,
+        required:true
+    }
+});
+
+const Project = model<IProject>("Project", projectSchema);
+export default Project;
